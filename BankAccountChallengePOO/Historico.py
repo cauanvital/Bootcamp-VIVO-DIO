0420cfd32ac8179e9714bce237c863c5178ac7fb
Cauan Vital <123972992+cauanvital@users.noreply.github.com>
2024-05-31 10:20:47 -0300

    Delete BankAccountChallengePOO directory

--- BankAccountChallengePOO/Historico.py
+++
@@ -1,5 +0,0 @@
-from Transacao import Transacao
-
-class Historico:
-    def adicionar_transacao(self, transacao:Transacao):
-        pass
--- BankAccountChallengePOO/Transacao.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from Conta import Conta
-
-class Transacao(ABC):
-    def registrar(self, conta:Conta):
-        return str(self)
-    
