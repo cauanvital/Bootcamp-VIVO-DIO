f415de7de278ed63f80f22020347cb13e2879394
Cauan Vital <123972992+cauanvital@users.noreply.github.com>
2024-07-18 12:58:16 -0300

    Add files via upload

---
+++ TDD API Project/store/schemas/product.py
@@ -0,0 +1,37 @@
+from decimal import Decimal
+from typing import Annotated, Optional
+from bson import Decimal128
+from pydantic import AfterValidator, BaseModel, Field
+from store.schemas.base import BaseSchemaMixin, OutMixin
+
+
+class ProductBase(BaseModel):
+    name: str = Field(..., description="Product name")
+    quantity: int = Field(..., description="Product quantity")
+    price: Decimal = Field(..., description="Product price")
+    status: bool = Field(..., description="Product status")
+
+
+class ProductIn(ProductBase, BaseSchemaMixin):
+    ...
+
+
+class ProductOut(ProductIn, OutMixin):
+    ...
+
+
+def convert_decimal_128(value):
+    return Decimal128(str(value))
+
+
+Decimal_ = Annotated[Decimal, AfterValidator(convert_decimal_128)]
+
+
+class ProductUpdate(ProductBase):
+    quantity: Optional[int] = Field(None, description="Product quantity")
+    price: Optional[Decimal_] = Field(None, description="Product price")
+    status: Optional[bool] = Field(None, description="Product status")
+
+
+class ProductUpdateOut(ProductUpdate, OutMixin):
+    ...
---
+++ TDD API Project/tests/schemas/test_product.py
@@ -0,0 +1,27 @@
+from pydantic import ValidationError
+import pytest
+from store.schemas.product import ProductIn
+from tests.factories import product_data
+
+
+def test_schemas_return_success():
+    data = product_data()
+    product = ProductIn.model_validate(data)
+
+    assert product.name == "Iphone 14 Pro Max"
+
+
+def test_schemas_return_raise():
+    data = product_data()
+    data.pop("status")
+
+    with pytest.raises(ValidationError) as err:
+        ProductIn.model_validate(data)
+
+    assert err.value.errors()[0] == {
+        "type": "missing",
+        "loc": ("status",),
+        "msg": "Field required",
+        "input": {"name": "Iphone 14 Pro Max", "quantity": 10, "price": "8.500"},
+        "url": "https://errors.pydantic.dev/2.8/v/missing",
+    }
